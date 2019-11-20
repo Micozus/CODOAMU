13df4d6d7bbeb46bf7376fd6c6da967de98124b8
lfiedor <lfiedoro@users.noreply.github.com>
2019-11-20 22:01:48 +0100

    MVP for recipe looking
    Added basic call to find recipes by ingredients.
    Dummy string substitution s/ /,/ but api is cool with this.

--- main.js
+++ main.js
@@ -4,25 +4,46 @@ const banner = document.querySelector(".banner");
 const recipes = document.querySelector(".recipes");
 
 const createRecipe = json => {
-  for (let i = 0; i < json.products.length; i++) {
+  for (recipe of json) {
     const newRecipe = document.createElement("article");
     newRecipe.classList.add("recipe");
     newRecipe.classList.add("container");
-    newRecipe.innerHTML = `<div class="recipe__image" style="background-image: url(${json.products[i].image})"></div>
-    <a href="#" class="recipe__description">${json.products[i].title}</a>`;
+    newRecipe.innerHTML = `<div class="recipe__image" style="background-image: url(${recipe.image})"></div>
+    <a href="#" class="recipe__description">${recipe.title}</a>`;
     recipes.appendChild(newRecipe);
   }
 };
+
+// button.addEventListener("click", e => {
+//   e.preventDefault();
+//   console.log(input.value);
+//   if (input.value) {
+//     fetch(
+//       `https://api.spoonacular.com/food/products/search?query=${input.value}&apiKey=2b3c299fe32948c1b09544ea44078485`
+//     )
+//       .then(response => response.json())
+//       .then(json => {
+//         createRecipe(json);
+//         console.log(json);
+//       });
+//     console.log(input.value);
+//     if (!banner.classList.contains("off")) {
+//       banner.classList.add("off");
+//     }
+//     input.value = null;
+//   }
+// });
+
 button.addEventListener("click", e => {
   e.preventDefault();
+  console.log(input.value);
   if (input.value) {
-    fetch(
-      `https://api.spoonacular.com/food/products/search?query=${input.value}&apiKey=2b3c299fe32948c1b09544ea44078485`
-    )
+    ingr = input.value.replace(new RegExp(' ', 'g'), ',');
+    fetch(`https://api.spoonacular.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=${ingr}&apiKey=649be07875ee49d9ac67a87858375775`)
       .then(response => response.json())
       .then(json => {
-        createRecipe(json);
         console.log(json);
+        createRecipe(json);
       });
     console.log(input.value);
     if (!banner.classList.contains("off")) {
@@ -31,3 +52,4 @@ button.addEventListener("click", e => {
     input.value = null;
   }
 });
+

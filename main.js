d218869c8328fb451c83ed33b693962f3260a88b
Piotr Czajkowski <sixtyto@gmail.com>
2019-11-20 06:59:37 +0100

    add API key

--- main.js
+++ main.js
@@ -4,26 +4,30 @@ const banner = document.querySelector(".banner");
 const recipes = document.querySelector(".recipes");
 
 const createRecipe = json => {
-  for (let i = 0; i < json.length; i++) {
+  for (let i = 0; i < json.products.length; i++) {
     const newRecipe = document.createElement("article");
     newRecipe.classList.add("recipe");
     newRecipe.classList.add("container");
-    newRecipe.innerHTML = `<div class="recipe__image" style="background-image: url(https://picsum.photos/id/${i}/100/100)"></div>
-    <a href="#" class="recipe__description">${json[i].title}</a>`;
+    newRecipe.innerHTML = `<div class="recipe__image" style="background-image: url(${json.products[i].image})"></div>
+    <a href="#" class="recipe__description">${json.products[i].title}</a>`;
     recipes.appendChild(newRecipe);
   }
 };
 button.addEventListener("click", e => {
   e.preventDefault();
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(response => response.json())
-    .then(json => {
-      createRecipe(json);
-      console.log(json);
-    });
-  console.log(input.value);
-  if (!banner.classList.contains("off")) {
-    banner.classList.add("off");
+  if (input.value) {
+    fetch(
+      `https://api.spoonacular.com/food/products/search?query=${input.value}&apiKey=2b3c299fe32948c1b09544ea44078485`
+    )
+      .then(response => response.json())
+      .then(json => {
+        createRecipe(json);
+        console.log(json);
+      });
+    console.log(input.value);
+    if (!banner.classList.contains("off")) {
+      banner.classList.add("off");
+    }
+    input.value = null;
   }
-  input.value = null;
 });

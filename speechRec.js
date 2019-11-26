33fac551f04d8007e1ba1c72ff9d4e583e810565
lfiedor <lfiedoro@users.noreply.github.com>
2019-11-26 20:01:52 +0100

    Recipe looking
    Simple api call to get recipes list

--- speechRec.js
+++ speechRec.js
@@ -13,14 +13,18 @@ ingList.appendChild(ingred);
 //pojemnik na składniki
 //todo dodawanie składników wprowadzonych tekstowo, a nie głosowo
 //todo usuwanie składników z listy -> evetListener?
-const ingredArray = [];
-
+const ingredArray = ['potato', 'chicken', 'salt', 'peppers', 'chilly'];
 
 //kończy to wciśnięcie przycisku Let's cook;
 //todo przekazanie listy do api
-const cookButton = document.getElementById("cookingTime");
 const handleStartCooking = () => {
-    alert(`finding recipes including: ${ingredArray.toString()}`);
+    // alert(`finding recipes including: ${ingredArray.toString()}`);
+    ingr = ingredArray.toString().replace(new RegExp(' ', 'g'), ',');
+    fetch(`https://api.spoonacular.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=${ingr}&apiKey=649be07875ee49d9ac67a87858375775`)
+        .then(response => response.json())
+        .then(json => {
+            showRecipes(json);
+        });
 };
 
 const handleFinalInput = (ingredient) => {
@@ -36,7 +40,7 @@ function handleDoneEnteringIngredients() {
     recognition.removeEventListener('end', recognition.start);
     // recognition.removeEventListener('result', handleInpuResult);
     recognition.removeEventListener('result', handleInpuResult);
-
+    
     // recognition.abort();
     console.log("should stop listening now");
     //emit eventu czy przekazanie od razu do szukania przepisów?
@@ -45,11 +49,11 @@ function handleDoneEnteringIngredients() {
 
 const handleInpuResult = (e) => {
     let ingredient = Array.from(e.results)
-        .map(result => result[0])
-        .map(result => result.transcript)
-        .join('');
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
     ingred.textContent = ingredient;
-
+    
     if (e.results[0].isFinal) {
         if (ingredient === "let's cook") {
             //todo zmienić na async, zeby najpierw text się czyścił
@@ -67,12 +71,23 @@ const handleInpuResult = (e) => {
 const button = document.getElementById("start_stop");
 button.addEventListener('click', () => {
     recognition.addEventListener('result', handleInpuResult);
-//ponowne uruchomienie nasłuchu po przyjęciu skłądnika
+    //ponowne uruchomienie nasłuchu po przyjęciu skłądnika
     recognition.addEventListener('end', recognition.start);
     //rozpoczęcie nasłuchu
     recognition.start();
 });
 
 //obsługa zakończenia wprowadania składników,
+const cookButton = document.getElementById("cookingTime");
 cookButton.addEventListener('click', handleStartCooking);
 
+const showRecipes = (recipes) => {
+    console.log(recipes);
+    const recipeList = document.querySelector('.card__recipe-list');
+    for(r of recipes) {
+        console.log(r);
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(r.title));
+        recipeList.appendChild(li)
+    }
+}

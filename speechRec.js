8be5625ee787264e2e1124760c902e5f96a69ca6
Ewelina <ewelinaformella@gmail.com>
2019-11-26 21:05:32 +0100

    some clean up done,and now user can add not more than 6 ingredients

--- speechRec.js
+++ speechRec.js
@@ -6,9 +6,11 @@ recognition.lang = 'en-US';
 
 //tworzę pojedynczy pojemnik na składnik
 let ingred = document.createElement('li');
-ingred.setAttribute("class", "card__list-item"); //zmienić li na odpowiedni tag ? chips ?
+ingred.setAttribute("class", "card__list-item");
+//zmienić li na odpowiedni tag ? chips ?
+// ingred = document.createElement('div');
+// ingred.setAttribute("class", "mdc-chip");
 const ingList = document.querySelector('.card__list');
-ingList.appendChild(ingred);
 
 //pojemnik na składniki
 //todo dodawanie składników wprowadzonych tekstowo, a nie głosowo
@@ -16,11 +18,11 @@ ingList.appendChild(ingred);
 const ingredArray = [];
 
 //kończy to wciśnięcie przycisku Let's cook;
-//todo przekazanie listy do api
 const handleStartCooking = () => {
     // alert(`finding recipes including: ${ingredArray.toString()}`);
     ingr = ingredArray.toString().replace(new RegExp(' ', 'g'), ',');
-    fetch(`https://api.spoonacular.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=${ingr}&apiKey=649be07875ee49d9ac67a87858375775`)
+    fetch(`https://api.spoonacular.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=
+    false&ingredients=${ingr}&apiKey=649be07875ee49d9ac67a87858375775`)
         .then(response => response.json())
         .then(json => {
             showRecipes(json);
@@ -33,6 +35,8 @@ const handleFinalInput = (ingredient) => {
 
     ingred = document.createElement('li');
     ingred.setAttribute("class", "card__list-item");
+    // ingred = document.createElement('div');
+    // ingred.setAttribute("class", "mdc-chip");
     ingList.appendChild(ingred);
 };
 
@@ -42,7 +46,7 @@ const removeSpeechListeners = () => {
 };
 
 const handleDoneEnteringIngredients = () => {
-    //todo zmienić na async, zeby najpierw text się czyścił
+    ingList.removeChild(ingred);
     ingred.textContent = '';//usuwa let's cook z listy wyświetlanych składników
     //odpinam nasłuch
     removeSpeechListeners();
@@ -50,22 +54,21 @@ const handleDoneEnteringIngredients = () => {
     handleStartCooking();
 };
 
-// const promise = new Promise(function (resolve, reject) {
-//     resolve(ingred.textContent = '');
-// });
-
-
 const handleInpuResult = (e) => {
+    //sprawdzam, czy mam miejsce
+    ingList.appendChild(ingred);
     let ingredient = Array.from(e.results)
-    .map(result => result[0])
-    .map(result => result.transcript)
-    .join('');
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
     ingred.textContent = ingredient;
-    
+
     if (e.results[0].isFinal) {
         if (ingredient === "let's cook") {
-            // promise.then(removeSpeechListeners).then(handleStartCooking);
             handleDoneEnteringIngredients();
+        } else if (ingredArray.length === 6) {
+            alert("Can't add more than 6 ingredients! Proceed to recipes " +
+                "or replace ingredients with another");
         } else {
             handleFinalInput(ingredient);
         }
@@ -90,10 +93,10 @@ cookButton.addEventListener('click', handleDoneEnteringIngredients);
 const showRecipes = (recipes) => {
     console.log(recipes);
     const recipeList = document.querySelector('.card__recipe-list');
-    for(r of recipes) {
+    for (r of recipes) {
         console.log(r);
         let li = document.createElement("li");
         li.appendChild(document.createTextNode(r.title));
         recipeList.appendChild(li)
     }
-}
+};

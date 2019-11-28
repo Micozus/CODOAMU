2c0be5b33646d5f7c7411e1a8c830cf3d5bad4c3
Paweł Mikos <51284757+Micozus@users.noreply.github.com>
2019-11-28 21:18:28 +0100

    -- Organize workplace -- Start working on layout

---
+++ src/js/models/main.js
@@ -0,0 +1,95 @@
+const button = document.querySelector(".search__button");
+const input = document.querySelector(".search__input > input");
+const banner = document.querySelector(".banner");
+const recipes = document.querySelector(".recipes");
+const card = document.querySelector(".card");
+
+const createRecipe = json => {
+  recipes.innerHTML = "";
+  // for (let i = 0; i < json.products.length; i++) {
+  const newRecipe = document.createElement("article");
+  //   newRecipe.dataset.id = json.products[i].id;
+  newRecipe.dataset.id = json; // do wywalenia
+  newRecipe.classList.add("recipe");
+  // newRecipe.innerHTML = `<div class="recipe__image" data-id="${json.products[i].id}" style="background-image: url(${json.products[i].image})"></div>
+  newRecipe.innerHTML = `<div class="recipe__image" data-id="${json}" style="background-image: url(${json})"></div>
+  <p data-id="${json}" class="recipe__description">${json}</p>`;
+  recipes.appendChild(newRecipe);
+  // }
+};
+button.addEventListener("click", e => {
+  e.preventDefault();
+  console.log(input.value);
+  if (input.value) {
+    //   fetch(
+    //     `https://api.spoonacular.com/food/products/search?query=${input.value}&apiKey=2b3c299fe32948c1b09544ea44078485`
+    //   )
+    //     .then(response => response.json())
+    //     .then(json => {
+    //       createRecipe(json);
+    //       console.log(json);
+    //     });
+    //   console.log(input.value);
+    if (!banner.classList.contains("off")) {
+      banner.classList.add("off");
+    }
+    if (recipes.classList.contains("off")) {
+      recipes.classList.remove("off");
+    }
+    if (!card.classList.contains("off")) {
+      card.classList.add("off");
+    }
+    createRecipe(input.value);
+    button.innerHTML = "Let's cook again!";
+  }
+});
+
+recipes.addEventListener("click", e => {
+  console.log(e.target);
+  card.innerHTML = "";
+  const newRecipeSummary = document.createElement("div");
+  newRecipeSummary.classList.add("card__wrapper");
+  newRecipeSummary.innerHTML = `
+  <h1 class="card__title">${e.target.dataset.id}</h1>
+  <div class="card__image"></div>
+  <div>
+    <p class="card__subtitle">ingredients:</p>
+      <ul class="card__list">
+        <li class="card__list-item">placeholder</li>
+        <li class="card__list-item">placeholder</li>
+        <li class="card__list-item">placeholder</li>
+        <li class="card__list-item">placeholder</li>
+        <li class="card__list-item">placeholder</li>
+      </ul>
+  </div>
+  <div>
+    <p class="card__subtitle">recipe:</p>
+      <ol class="card__list">
+        <li class="card__list-item">placeholder</li>
+        <li class="card__list-item">placeholder</li>
+        <li class="card__list-item">placeholder</li>
+        <li class="card__list-item">placeholder</li>
+      </ol>
+    </div>
+  `;
+  recipes.classList.toggle("off");
+  card.appendChild(newRecipeSummary);
+  card.classList.toggle("off");
+});
+
+input.addEventListener("keyup", e => {
+  if (e.code == "Comma") {
+    const chips = [];
+    input.value.split(",").forEach(element => {
+      if (element) {
+        chips.push(`<span class="mdl-chip mdl-chip--deletable">
+      <span class="mdl-chip__text">${element}</span>
+      <button type="button" class="mdl-chip__action"><i class="material-icons">cancel</i></button>
+  </span>`);
+      }
+    });
+    console.log("loop");
+    document.querySelector(".chips").innerHTML = chips.join("");
+  }
+  console.log(input.value);
+});
---
+++ src/js/models/speechRec.js
@@ -0,0 +1,191 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+
+//tworzę pojedynczy pojemnik na składnik - tymczasowo listItem
+let ingred = document.createElement('li');
+ingred.setAttribute("class", "card__list-item");
+//zmienić li na odpowiedni tag ? chips ?
+// ingred = document.createElement('div');
+// ingred.setAttribute("class", "mdc-chip");
+//pojemnik na składniki
+const ingList = document.querySelector('.card__list');
+
+//todo dodawanie składników wprowadzonych tekstowo, a nie głosowo
+//todo usuwanie składników z listy -> eventListener?
+const ingredArray = [];
+
+//todo nie click, a 'sluchamCoWyemitujeChipsPoKliknięciuX' czy jakośtam
+//tą funkcję mogę przetestowac dopiero na koniec, narazie nie mam jak
+const removeFromList = (e) => {
+    let elementToRemove = e.target.textContent;
+    if (ingredArray.includes(elementToRemove, 0)) {
+        let indexToRemove = ingList.indexOf(elementToRemove);
+        ingList.splice(indexToRemove, 1);
+    }
+};
+ingList.addEventListener('click', removeFromList);
+
+
+//kończy to wciśnięcie przycisku Let's cook;
+const handleStartCooking = () => {
+    // alert(`finding recipes including: ${ingredArray.toString()}`);
+    ingr = ingredArray.toString().replace(new RegExp(' ', 'g'), ',');
+    fetch(`https://api.spoonacular.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=
+    false&ingredients=${ingr}&apiKey=649be07875ee49d9ac67a87858375775`)
+        .then(response => response.json())
+        .then(json => {
+            getRecipes(json);
+        });
+};
+
+const handleFinalInput = (ingredient) => {
+    //pushuje składnik do listy tylko gdy jest final
+    ingredArray.push(ingredient);
+
+    ingred = document.createElement('li');
+    ingred.setAttribute("class", "card__list-item");
+    // ingred = document.createElement('div');
+    // ingred.setAttribute("class", "mdc-chip");
+    ingList.appendChild(ingred);
+};
+
+const removeSpeechListeners = () => {
+    recognition.removeEventListener('end', recognition.start);
+    recognition.removeEventListener('result', handleInpuResult);
+};
+
+const handleDoneEnteringIngredients = () => {
+    ingList.removeChild(ingred);
+    ingred.textContent = '';//usuwa let's cook z listy wyświetlanych składników
+    //odpinam nasłuch
+    removeSpeechListeners();
+    //emit eventu czy przekazanie od razu do szukania przepisów?
+    handleStartCooking();
+};
+
+const handleInpuResult = (e) => {
+    //sprawdzam, czy mam miejsce na kolejny składnik
+    ingList.appendChild(ingred);
+    let ingredient = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+    ingred.textContent = ingredient;
+
+    if (e.results[0].isFinal) {
+        if (ingredient === "let's cook") {
+            handleDoneEnteringIngredients();
+        } else if (ingredArray.length === 6) {
+            alert("Can't add more than 6 ingredients! Proceed to recipes " +
+                "or replace ingredients with another");
+        } else {
+            handleFinalInput(ingredient);
+        }
+    }
+};
+
+//pobieranie tekstu
+//obsługa przycisku do rozpoczęcia przyjmowania skłądników
+//dopiero do wciśnięciu, aplikacja zaczyna nasłuchiwać
+const listenButton = document.querySelector(".listen_button");
+
+const startListening = () => {
+    recognition.addEventListener('result', handleInpuResult);
+    //ponowne uruchomienie nasłuchu po przyjęciu skłądnika
+    recognition.addEventListener('end', recognition.start);
+    //rozpoczęcie nasłuchu
+    recognition.start();
+};
+
+listenButton.addEventListener('click', startListening);
+
+//obsługa zakończenia wprowadania składników -> id cooking time zamienić na włąściwe
+// id buttona rozpoczynającego szukania
+const cookButton = document.querySelector(".search__button");
+
+
+cookButton.addEventListener('click', handleDoneEnteringIngredients);
+
+const getRecipes = (recipes) => {
+    console.log(recipes);
+    for (r of recipes) {
+        fetch(`https://api.spoonacular.com/recipes/${r.id}/information?includeNutrition=false&apiKey=649be07875ee49d9ac67a87858375775`)
+            .then(response => response.json())
+            .catch(new Error('Could not get recipe info'))
+            .then(json => {
+                getRecipeSteps(json);
+            });
+    }
+};
+
+const getRecipeSteps = (recipe) => {
+    fetch(`https://api.spoonacular.com/recipes/${recipe.id}/analyzedInstructions?stepBreakdown=false&apiKey=649be07875ee49d9ac67a87858375775`)
+        .then(response => response.json())
+        .catch(new Error('Could not get steps info'))
+        .then(json => {
+            console.log(json);
+            createRecipeEntry(recipe, json);
+        });
+}
+
+const createRecipeEntry = (recipeJson, stepsJson) => {
+    const recipeList = document.querySelector('.card__recipe-list');
+    //<li class="card__recipe-list-item">
+    //  <div class="card__wrapper">
+    //   <h1 class="card__title">placeholder</h1>
+    //   <div class="card__image"></div>
+    //   <section class="card__ingredients">ingredients:
+    //       <ul class="card__list">
+    //              <li class="card__list-item">placeholder</li>
+    //       </ul>
+    //   </section>
+    //   <section class="card__recipe">
+    //       <ol class="card__recipe-list">
+    //              <li class="card__recipe-list-item">placeholder</li>
+    //          </ol>
+    //      </section>
+    //  </div>
+    //</li>
+    let li = document.createElement("li");
+    let div_c_wrap = document.createElement("div");
+    li.appendChild(div_c_wrap);
+
+    let h1_c_title = document.createElement("h1");
+    h1_c_title.textContent = recipeJson.title;
+    div_c_wrap.appendChild(h1_c_title);
+
+    let div_c_img = document.createElement('div');
+    div_c_wrap.appendChild(div_c_img);
+
+    let section_c_ing = document.createElement('section');
+    section_c_ing.textContent = 'Ingredients:';
+    div_c_wrap.appendChild(section_c_ing);
+
+    let ul_c_list = document.createElement('ul');
+    section_c_ing.appendChild(ul_c_list);
+
+    for (let ing of recipeJson.extendedIngredients) {
+        let li_c_list_item = document.createElement("li");
+        li_c_list_item.appendChild(document.createTextNode(ing.name));
+        ul_c_list.appendChild(li_c_list_item)
+    }
+
+    let section_c_step = document.createElement('section');
+    section_c_step.textContent = 'Steps:';
+    div_c_wrap.appendChild(section_c_step);
+
+    let ul_c_step_list = document.createElement('ul');
+    section_c_step.appendChild(ul_c_step_list);
+
+    for (let section of stepsJson) {
+        for (let step of section.steps) {
+            let li_c_list_item = document.createElement("li");
+            li_c_list_item.appendChild(document.createTextNode(step.step));
+            ul_c_step_list.appendChild(li_c_list_item);
+        }
+    }
+    recipeList.appendChild(li);
+};

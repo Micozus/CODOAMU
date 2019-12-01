b5feb6229da76780e1fa5a83999b57f7726d671e
Paweł Mikos <51284757+Micozus@users.noreply.github.com>
2019-12-01 17:55:01 +0100

    Dist added

---
+++ precache-manifest.be02f8673f07608f442740f634480f62.js
@@ -0,0 +1,99 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b397254ca5bbb2e34a8b2804393c7f0",
+    "url": "favicon.ico"
+  },
+  {
+    "url": "icon_128x128.eada429bb90fae4079aa6af6343b1b28.png"
+  },
+  {
+    "url": "icon_144x144.57640d5abf9ff5328a9f4c758834f747.png"
+  },
+  {
+    "url": "icon_152x152.2eaedbd0d7890bc9a56333a8162ea1d5.png"
+  },
+  {
+    "url": "icon_192x192.75f7ce352be668ee2afac87e0843d51f.png"
+  },
+  {
+    "url": "icon_384x384.5895108dd3fa6ee2cd0b59d05783289b.png"
+  },
+  {
+    "url": "icon_512x512.34790fcc658769a95b4a98c6502d781a.png"
+  },
+  {
+    "url": "icon_72x72.483fe92bc6a6bb64c12a8ea5713bead7.png"
+  },
+  {
+    "url": "icon_96x96.ce3148266499b7bb79d2ea5c39056fae.png"
+  },
+  {
+    "url": "images/check-mark.5ea9e95972312b1774ebeade88f415a4.png"
+  },
+  {
+    "url": "images/chef.765522ca0c27a3ee697e9ca94056c0d5.png"
+  },
+  {
+    "url": "images/dish-prepare.6da4b2a2beac351b5ef504082fb7076d.png"
+  },
+  {
+    "url": "images/fridge-1.dec95c2f2a9d6278fc784e9906c3ecc8.png"
+  },
+  {
+    "url": "images/fridge-open.8b113a38136b6af823662c0b556814b2.png"
+  },
+  {
+    "url": "images/harvest.f55234844a1edfdf943e134ad9ea18c9.png"
+  },
+  {
+    "url": "images/kitchen-ele-1.ac10c74cb0ef2a6e1b2ced5b832c9dc4.png"
+  },
+  {
+    "url": "images/kitchen-ele-2.01549704f3af141da24cf115e1c8c730.png"
+  },
+  {
+    "url": "images/kitchen-ele-4.b1136202d3c358115faba22ab94508b8.png"
+  },
+  {
+    "url": "images/kitchen-ele-5.3283cc55df749ee13edb70bf94717ba5.png"
+  },
+  {
+    "url": "images/kitchen-ele-6.715c3818e0ba07417a4dad5353645bc3.png"
+  },
+  {
+    "url": "images/logo.0ccf49b00727606f1f985d9fd5d34cb3.png"
+  },
+  {
+    "url": "images/microwave.7423639c1a0368de35649c0a4c6411ff.png"
+  },
+  {
+    "url": "images/wall_texture_bg.5066b5255a56d83f48497eb2fe144257.png"
+  },
+  {
+    "revision": "539aa5caa9a1779ca971bb4a806af003",
+    "url": "index.html"
+  },
+  {
+    "revision": "30924032fa857fe220d2",
+    "url": "main.1fbe81b9cf199a1cd1c9.bundle.js"
+  },
+  {
+    "revision": "30924032fa857fe220d2",
+    "url": "main.2edef182efd908c99384.css"
+  },
+  {
+    "url": "manifest.26af7aa46b07b5f18d09463dee2c29eb.json"
+  },
+  {
+    "revision": "388ed88eec82ddeacbf877ee7dc4b225",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "e1bf10ca73bb28063502c513a99485e6",
+    "url": "sitemap.xml"
+  },
+  {
+    "revision": "0ca75386919700082461",
+    "url": "vendor.020c41c50ea34a8665f1.bundle.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,4 @@
+importScripts("precache-manifest.be02f8673f07608f442740f634480f62.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
